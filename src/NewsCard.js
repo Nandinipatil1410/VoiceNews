84ed7d0913733ea020c906fd892c2b2f53c3acc5
Nandinipatil1410 <nandinipatil1410@gmail.com>
2024-11-15 00:51:25 +0530

    Added resources & Modified NewsCard.js

--- src/NewsCard.js
+++ src/NewsCard.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import './NewsCardStyle.css';
+import defaultImage from './resources/default-image.jpg'; 
 
-const NewsCard = ({ title, description, link, imageUrl }) => {
+
+const NewsCard = ({ title, description, url, imageUrl }) => {
+  const imageToDisplay = imageUrl ? imageUrl : defaultImage; 
+  
   return (
     <div className="card">
-      {imageUrl && <img src={imageUrl} alt={title} />}
+      <img src={imageToDisplay} alt={title} />
       <div className="card-content">
         <h3>{title}</h3>
         <p>{description}</p>
-        <a href={link} target="_blank" rel="noopener noreferrer">
+        <a href={url} target="_blank" rel="noopener noreferrer">
           Read more
         </a>
       </div>
     </div>
   );
 };
+
 
 export default NewsCard;
